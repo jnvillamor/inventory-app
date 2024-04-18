0e22058e82ac4546362eabeddbbc8fddbeb77a9f
-m <villamorjohnnoel@gmail.com>
2024-04-19 00:34:40 +0800

    feature: enable login using google account

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,6 +3,7 @@ import User from '@/models/user';
 import { compare } from 'bcrypt';
 import nextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
 
 const handler = nextAuth({
   providers: [
@@ -28,8 +29,54 @@ const handler = nextAuth({
           console.log(error);
         }
       }
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
-  ]
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email
+      })
+      
+      // update the session
+      session.user.id = sessionUser._id.toString();
+      session.firstName = sessionUser.firstName;
+      session.lastName = sessionUser.lastName;
+      
+      return session;
+    },
+
+    async signIn({ account, profile }) {
+      if (account?.provider == 'credentials') {
+        return true;
+      }
+      
+      try {
+        await connectToDb();
+
+        // check if user exists
+        const userExist = await User.findOne({ email: profile.email });
+
+        // create new user if user does not exists
+        if(!userExist) {
+          await User.create({
+            email: profile.email,
+            image: profile.picture,
+            lastName: profile.family_name,
+            firstName: profile.given_name,
+          })
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    }
+  }
 });
 
 export { handler as GET, handler as POST };
