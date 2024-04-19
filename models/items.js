6b487b17fb787ed083cdcc1f000aa2ed18938a93
-m <villamorjohnnoel@gmail.com>
2024-04-20 01:24:38 +0800

    Defined the item schema

---
+++ models/items.js
@@ -0,0 +1,31 @@
+import { Schema, model, models } from 'mongoose';
+
+const ItemSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    item_name: {
+      type: String,
+      require: [true, 'Item name is required']
+    },
+    description: {
+      type: String,
+      require: [true, 'Description is required']
+    },
+    quantity: {
+      type: Number,
+      require: [true, 'Quantity is required']
+    },
+    unit_price: {
+      type: Number,
+      require: [true, 'Unit price is required']
+    }
+  },
+  { timestamps: true }
+);
+
+const Item = models.Item || model('Item', ItemSchema);
+
+export default Item;
