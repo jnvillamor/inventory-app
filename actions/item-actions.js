e42d2e4861f320fff5918c3593474bad476365cc
-m <villamorjohnnoel@gmail.com>
2024-04-20 17:40:49 +0800

    fix: use actions only instead of using both server actions and route handler

--- actions/item-actions.js
+++ actions/item-actions.js
@@ -1,26 +1,40 @@
 'use server';
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { connectToDb } from '@/lib/mongodb';
+import { getServerSession } from 'next-auth';
+import Item from '@/models/items';
 
 export const create_item = async (formdata) => {
   const session = await getServerSession(authOptions);
-  
-  const res = await fetch('http://localhost:3000/api/item', {
-    method: 'POST',
-    body: JSON.stringify({
+
+  if (!session.user) {
+    return {
+      success: false,
+      message: 'You must be logged in'
+    };
+  }
+
+  try {
+    await connectToDb();
+
+    const newItem = new Item({
       owner: session.user.id,
       item_name: formdata.get('item_name'),
-      desc: formdata.get('desc'),
+      description: formdata.get('desc'),
       quantity: formdata.get('quantity'),
-      unit_price: formdata.get('unit_price')
-    })
-  })
+      unitPrice: formdata.get('unit_price')
+    });
 
-  // TODO: Revise this part according to what action should be done after
-  // sending the request
-  if(res.ok) {
-    redirect('/');
+    newItem.save();
+    return {
+      success: true,
+      message: 'Item created successfully.'
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: 'Something went wrong'
+    };
   }
 };
