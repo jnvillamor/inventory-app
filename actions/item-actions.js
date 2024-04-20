674ec1842de649eee5cc80f091871d836fb79c73
-m <villamorjohnnoel@gmail.com>
2024-04-20 19:02:32 +0800

    fix: all fields are store correctly

--- actions/item-actions.js
+++ actions/item-actions.js
@@ -3,12 +3,14 @@
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { connectToDb } from '@/lib/mongodb';
 import { getServerSession } from 'next-auth';
+import { revalidatePath } from 'next/cache';
 import Item from '@/models/items';
 
+// CREATE
 export const create_item = async (formdata) => {
   const session = await getServerSession(authOptions);
 
-  if (!session.user) {
+  if (!session?.user) {
     return {
       success: false,
       message: 'You must be logged in'
@@ -17,16 +19,18 @@ export const create_item = async (formdata) => {
 
   try {
     await connectToDb();
-
+    
     const newItem = new Item({
       owner: session.user.id,
-      item_name: formdata.get('item_name'),
+      itemName: formdata.get('item_name'),
+      unitPrice: formdata.get('unit_price'),
       description: formdata.get('desc'),
-      quantity: formdata.get('quantity'),
-      unitPrice: formdata.get('unit_price')
+      quantity: formdata.get('quantity')
     });
 
     newItem.save();
+    revalidatePath('/create-item');
+    
     return {
       success: true,
       message: 'Item created successfully.'
